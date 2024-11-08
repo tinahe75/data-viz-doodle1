a6975b5391bf9f9c620bab511e1c3754efff0da2
Tina He <heyan75@gmail.com>
2024-11-07 22:00:16 -0800

    initial commit

---
+++ test.js
@@ -0,0 +1,19 @@
+var selectedPlotName;
+
+function onIframeLoad() {
+    console.log("xxx")
+    const iFrameID = document.getElementById('plot-iframe');
+    if(iFrameID) {
+        // here you can make the height, I delete it first, then I make it again
+        iFrameID.height = "";
+        iFrameID.height = iFrameID.contentWindow.document.body.scrollHeight + "px";
+        iFrameID.width = iFrameID.contentWindow.document.body.scrollWidth + "px";
+    }
+}
+
+function onSelectPlot(plot) {
+    const iFrameID = document.getElementById('plot-iframe');
+    if(iFrameID) {
+        iFrameID.src = "plots/"+plot;
+    }
+}
