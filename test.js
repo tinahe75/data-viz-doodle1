ab9b6a92ae0979c30050d0f9fcc4a25736f36d82
Tina He <heyan75@gmail.com>
2024-11-12 16:13:45 -0800

    added interval plot

--- test.js
+++ test.js
@@ -1,16 +1,3 @@
-var selectedPlotName;
-
-function onIframeLoad() {
-    console.log("xxx")
-    const iFrameID = document.getElementById('plot-iframe');
-    if(iFrameID) {
-        // here you can make the height, I delete it first, then I make it again
-        iFrameID.height = "";
-        iFrameID.height = iFrameID.contentWindow.document.body.scrollHeight + "20px";
-        iFrameID.width = iFrameID.contentWindow.document.body.scrollWidth + "20px";
-    }
-}
-
 function onSelectPlot(plot, displayId) {
     const iFrameID = document.getElementById(displayId);
     if(iFrameID) {
